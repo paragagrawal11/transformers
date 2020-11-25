c40ab84308827d0e20a1670aae688571e3736f47
Morgan Funtowicz <morgan@huggingface.co>
2020-11-25 15:52:26 +0100

    Restore correct output ordering...
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- src/transformers/modeling_flax_bert.py
+++ src/transformers/modeling_flax_bert.py
@@ -294,7 +294,7 @@ class FlaxBertModule(nn.Module):
         )(embeddings, attention_mask)
 
         pooled = FlaxBertPooler(name="pooler")(encoder)
-        return pooled, encoder
+        return encoder, pooled
 
 
 @add_start_docstrings(
@@ -503,7 +503,7 @@ class FlaxBertForMaskedLM(FlaxBertModel):
             jnp.array(position_ids, dtype="i4"),
         )
 
-        return pooled, logits
+        return logits, pooled
 
     def init_head(self, rng):
         fake_input_ids = jnp.ones((1, 10), dtype='i4')
